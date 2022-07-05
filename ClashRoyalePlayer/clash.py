771fc023570b2cb675ffb7348a0a14ceaf560c34
_Leg3ndary <ben.zhou.2006@gmail.com>
2022-07-05 10:12:52 -0400

    push clash updates

--- ClashRoyalePlayer/clash.py
+++ ClashRoyalePlayer/clash.py
@@ -10,39 +10,44 @@ import pyautogui as pag
 loop = asyncio.get_event_loop()
 
 
-async def end_match(match) -> None:
+async def end_match(match: bool) -> None:
     """
     End the match
     """
-    match = False
+    if match:
+        match = False
     pag.press("2")
+    print("Match ended.")
 
 
 async def place_card() -> None:
     """
-    Attempts to place a card
+    Attempts to place a card by selecting a card then waiting a random a random
+    amount of time then placing.
     """
     pag.press(str(random.randint(1, 4)))
-    await asyncio.sleep(0.3)
+    await asyncio.sleep(random.randint(1, 5) / 10)
     pag.click(random.randint(500, 830), random.randint(363, 570))
 
 
-async def check_match(match) -> None:
+async def check_match(match: bool) -> None:
     """
     Check the match's status
     """
     found = pag.locateOnScreen(
         "ClashRoyalePlayer/images/ok.png", region=(610, 650, 110, 60)
     )
-
     if found:
         await end_match(match)
 
 
 async def emote():
     """
-    Pick a emote
+    Pick a emote randomly lol
     """
+    pag.click(1, 1)
+    await asyncio.sleep(random.randint(1, 2) / 5)
+    pag.click()
 
 
 async def main():
@@ -73,7 +78,7 @@ async def main():
         await asyncio.sleep(3)
         print("Starting.")
         while True:
-            pag.click(748, 508)
+            pag.click(748, 508) # party
             await asyncio.sleep(random.randint(1, 3))
             pag.click(761, 551)
             match = True
@@ -83,8 +88,8 @@ async def main():
                 await asyncio.sleep(1)
                 await place_card()
                 await asyncio.sleep(random.randint(1, 6))
-                loop.create_task(check_match(match))
-            await asyncio.sleep("7")
+                await check_match(match)
+            await asyncio.sleep(6) # network, speed, whatever
 
 
 if __name__ == "__main__":
